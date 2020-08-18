5e339a56b90453dfea6b264654ecfac002fc80c6
Akash Kumar Singh <akash.akara@gmail.com>
2020-08-18 22:06:52 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import TypeWriter from 'react-native-typewriter'
+import { View, Text } from 'react-native'
+ 
+export default class App extends Component {
+  render() {
+    return(
+      <View style={{flex:1,backgroundColor:'black'}}>
+        <Text style={{fontSize:'20',color:'white',top:50,textAlign:'center'}}>React-Native-Typewriter</Text>
+        <View style={{top:200}}>
+      <TypeWriter   typing={1}><Text style={{fontSize:25,color:'white',alignContent:'center'}}>Hello Everyone! My name is <Text style={{color:'red'}}>Akash Kumar Singh.</Text>I am a React Native Developer and this is the example of React-Native-Typescript</Text></TypeWriter>
+     </View>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      </View>
+      );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
