7c9b54083ef0010ba47f71622e6e36f302fe3828
Akash Kumar Singh <akash.akara@gmail.com>
2020-08-18 22:29:31 +0530

    2nd

--- App.js
+++ App.js
@@ -6,9 +6,9 @@ export default class App extends Component {
   render() {
     return(
       <View style={{flex:1,backgroundColor:'black'}}>
-        <Text style={{fontSize:'20',color:'white',top:50,textAlign:'center'}}>React-Native-Typewriter</Text>
+        <Text style={{fontSize:20,color:'white',top:50,textAlign:'center'}}>React-Native-Typewriter</Text>
         <View style={{top:200}}>
-      <TypeWriter   typing={1}><Text style={{fontSize:25,color:'white',alignContent:'center'}}>Hello Everyone! My name is <Text style={{color:'red'}}>Akash Kumar Singh.</Text>I am a React Native Developer and this is the example of React-Native-Typescript</Text></TypeWriter>
+      <TypeWriter   typing={1}><Text style={{fontSize:25,color:'white',alignContent:'center'}}>Hello Everyone! My name is <Text style={{color:'red'}}>Akash Kumar Singh.</Text>I am a React Native Developer and this is the example of React-Native-Typewriter</Text></TypeWriter>
      </View>
 
       </View>
