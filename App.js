5004ecc00ad76cce12cbce17e6c637171ce07ebd
Akash Kumar Singh <akash.akara@gmail.com>
2020-08-19 19:58:12 +0530

    final

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react'
 import TypeWriter from 'react-native-typewriter'
-import { View, Text } from 'react-native'
- 
+import { Animated,View, Text ,Image} from 'react-native'
+import * as Animatable from 'react-native-animatable';
+
 export default class App extends Component {
+   fadeIn = {
+    0: {
+      opacity: 0,
+
+    },
+    0.5: {
+      opacity: 1,
+      
+    },
+    1: {
+      opacity: 0,
+      
+    },
+   
+  };
   render() {
     return(
-      <View style={{flex:1,backgroundColor:'black'}}>
-        <Text style={{fontSize:20,color:'white',top:50,textAlign:'center'}}>React-Native-Typewriter</Text>
-        <View style={{top:200}}>
-      <TypeWriter   typing={1}><Text style={{fontSize:25,color:'white',alignContent:'center'}}>Hello Everyone! My name is <Text style={{color:'red'}}>Akash Kumar Singh.</Text>I am a React Native Developer and this is the example of React-Native-Typewriter</Text></TypeWriter>
+      <View style={{flex:1,backgroundColor:'white'}}>
+        
+        <View style={{flexDirection:'row',top:300,backgroundColor:'black',width:300,height:60,left:35,borderRadius:15}}>
+          <Text style={{fontSize:35,left:10,color:'white',top:5}}>Downloading</Text>
+          <View style={{  backgroundColor:'white',height:50,width:60,left:35,top:5,borderRadius:10}}>
+            <View style={{flexDirection:'row',justifyContent:'space-around',top:23}}>
+          <Animatable.Text  animation={this.fadeIn} delay = {500} iterationCount='infinite' duration={3000}  style={{ textAlign: 'center' }}>
+          <Image style={{height:10,width:10,top:80}}
+        source={require('./assets/dot.png')}
+      />
+      
+          </Animatable.Text>
+          <Animatable.Text  animation={this.fadeIn} delay = {1100} duration={3000} iterationCount='infinite' style={{ textAlign: 'center' }}>
+          <Image style={{height:10,width:10,}}
+        source={require('./assets/dot.png')}
+      />
+          </Animatable.Text>
+          <Animatable.Text  animation={this.fadeIn} delay = {1700} iterationCount='infinite' duration={3000}  style={{ textAlign: 'center' }}>
+          <Image style={{height:10,width:10}}
+        source={require('./assets/dot.png')}
+      />
+          </Animatable.Text>
+          <Animatable.Text  animation={this.fadeIn} delay = {2300} iterationCount='infinite'  duration={3000} style={{ textAlign: 'center' }}>
+          <Image style={{height:10,width:10}}
+        source={require('./assets/dot.png')}
+      />
+          </Animatable.Text>
+          </View>
+          </View>
      </View>
 
       </View>
